3e08f68429f0d2c0b78b7fa1b1aae172581e05d7
exploitx632 <exploitx632hql@gmail.com>
2026-03-20 17:10:36 +0800

    added module-35

--- module-35(JavaScript You need to know for React)/script.js
+++ module-35(JavaScript You need to know for React)/script.js
@@ -1,1 +1,67 @@
-console.log('hello world')
+// const products = [
+//   { brand: "Apple", model: "iPhone 13 Pro", color: "Silver", price: 50000 },
+//   { brand: "Samsung", model: "S26 Ultra", color: "Black", price: 50000 },
+//   { brand: "Xaomi", model: "Xaomi Note 15 Pro", color: "White", price: 25000 },
+//   { brand: "OnePlus", model: "OnePlus 8 Pro", color: "Purple", price: 30000 }
+// ];
+
+// const result = products.map(product => console.log(product.brand))
+// const maxPrice = products.filter(product => product.price >= 50000)
+// console.log(maxPrice)
+// const arrowFunc = (a,b,...rest)=>{
+//     console.log(a,b)
+
+//     console.log(rest)
+// }
+
+// arrowFunc(1,2,3,4,5)
+
+// const friends = ["Hero Alom","Hena","Bapparaj","Kabila"]
+
+// const [hero,hena,Bapparaj,Kabila] = friends;
+// // console.log(hero,Bapparaj,Kabila)
+
+// const person = {
+//   name: "Shaon",
+//   age : 24,
+//   nationality: "Bangladeshi"
+// }
+
+// const {name,age,nationality}= person
+// console.log(name,age,nationality)
+
+// const person = {
+//   name: "Shaon",
+//   age : 24,
+//   nationality: "Bangladeshi"
+// }
+
+// const jsonData = JSON.stringify(person)
+// console.log(jsonData)
+// const jsonParse = JSON.parse(jsonData)
+// console.log(jsonParse)
+
+// fetch("url")
+// .then(respose => respose.json())
+// .then(data => console.log)
+
+// const products = [
+//   { brand: "Apple", model: "iPhone 13 Pro", color: "Silver", price: 50000 },
+//   { brand: "Samsung", model: "S26 Ultra", color: "Black", price: 50000 },
+//   { brand: "Xaomi", model: "Xaomi Note 15 Pro", color: "White", price: 25000 },
+//   { brand: "OnePlus", model: "OnePlus 8 Pro", color: "Purple", price: 30000 },
+// ];
+// const newProduct = {
+//   brand: "Walton",
+//   model: "Walton Phone",
+//   color: "blue",
+//   price: 15000,
+// };
+
+// const allProduct = [...products, newProduct];
+// // console.log(allProduct)
+
+// const filteredProducts = allProduct.filter(
+//   (product) => product.brand !== "Xaomi"
+// );
+// console.log(filteredProducts);
